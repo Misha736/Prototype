d2b600e205ddfd7dba6416fe9be1207d5608c311
Misha736 <nurislam.kalhaman@gmail.com>
2025-07-03 10:44:36 +0500

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,3 +1,6 @@
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 local humanoid = char:WaitForChild("Humanoid")
+humanoid.WalkSpeed = 200
+if Humanoid.WalkSpeed = 200 then
+humanoid.WalkSpeed = 20
